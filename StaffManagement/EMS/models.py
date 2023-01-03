3de393ef5f9285ee3daab6d13de4138f4cfd0e6f
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-01-03 03:37:47 +0330

    Update Model

--- StaffManagement/EMS/models.py
+++ StaffManagement/EMS/models.py
@@ -4,19 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Employee (models.Model):
-    user = models.ForeignKey(User,on_delete=True,max_length=50)
-    first_name = models.CharField(name='first_name',max_length=50)
-    last_name = models.CharField(name='last_name',max_length=50)
-    username = models.CharField(name='last_name',max_length=50)
-    national_id_number = models.IntegerField (name='id_no',max_length=10)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=False,max_length=50)
+    firstName = models.CharField(name='first_name',max_length=50)
+    lastName = models.CharField(name='last_name',max_length=50)
+    username = models.CharField(name='username',max_length=50)
+    national_id_number = models.IntegerField (name='id_no')
     # address = 
     # phone_number = 
-
-
-class Performance (models.Model):
-    arrive_time = models.DateTimeField(auto_now_add=True,auto_created=True)
-    leave_time = models.DateTimeField(auto_now_add=True,auto_created=True)
-    attendance = models.DateTimeField(auto_now_add=True,auto_created=True)
 
 class Request (models.Model): 
     employee = models.ForeignKey(Employee,on_delete=models.CASCADE,null=False,default='')
@@ -25,10 +19,6 @@ class Request (models.Model):
     is_remote =  models.BooleanField(null=True) # request for remote work
     description = models.TextField(max_length=100,verbose_name='توضیحات')
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-class Employee_Finance (models.Model): 
-    pass
 
 class Task(models.Model):
     title = models.CharField(max_length = 50)
@@ -39,7 +29,3 @@ class Task(models.Model):
     time_end = models.TimeField(verbose_name='زمان پایان',null=False,default='')
     description = models.TextField(max_length=100,verbose_name='توضیحات')
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-class Team () : 
-    pass 
