7a6938dcc17ff911998c15b53388d2c786a23b1c
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-01-03 03:37:20 +0330

    add models to admin

--- StaffManagement/EMS/admin.py
+++ StaffManagement/EMS/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Employee,Request,Task,User
 
 # Register your models here.
+
+admin.site.register(Employee)
+admin.site.register(Request)
+admin.site.register(Task)
