edcb455067efa2b4469cb625544b58806f8032fa
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-03-02 14:26:40 +0330

    update models

--- StaffManagement/EMS/models.py
+++ StaffManagement/EMS/models.py
@@ -9,16 +9,19 @@ class Employee (models.Model):
     lastName = models.CharField(name='last_name',max_length=50)
     username = models.CharField(name='username',max_length=50)
     national_id_number = models.IntegerField (name='id_no')
-    # address = 
-    # phone_number = 
+    # address =
+    # phone_number =
 
-class Request (models.Model): 
+class Request (models.Model):
     employee = models.ForeignKey(Employee,on_delete=models.CASCADE,null=False,default='')
     title = models.CharField(max_length = 50)
     date = models.DateField(verbose_name='تاریخ')
-    is_remote =  models.BooleanField(null=True) # request for remote work
+    is_remote = models.BooleanField(null=True) # request for remote work
+    is_leave = models.BooleanField(null=True) # request for leave
     description = models.TextField(max_length=100,verbose_name='توضیحات')
     created_at = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateField(verbose_name='تاریخ شروع')
+    end_date = models.DateField(verbose_name='تاریخ پایان')
 
 class Task(models.Model):
     title = models.CharField(max_length = 50)
@@ -29,3 +32,10 @@ class Task(models.Model):
     time_end = models.TimeField(verbose_name='زمان پایان',null=False,default='')
     description = models.TextField(max_length=100,verbose_name='توضیحات')
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Attendance (models.Model):
+    employee = models.ForeignKey(Employee,on_delete=models.CASCADE,null=False,default='')
+    date = models.DateField(verbose_name='تاریخ')
+    arrival_time = models.TimeField()
+    leaving_time = models.TimeField()
+    is_absent = models.BooleanField()
