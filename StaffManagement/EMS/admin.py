cb1151bb69b324775eff6da2858c2efb8e731c21
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-03-07 09:33:30 +0330

    delete old project

--- StaffManagement/EMS/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employee,Request,Task,User
-
-# Register your models here.
-
-admin.site.register(Employee)
-admin.site.register(Request)
-admin.site.register(Task)
