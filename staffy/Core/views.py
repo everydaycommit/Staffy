59447b52edc5f6f0b7011786e6531aeeb86a146e
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2023-03-09 10:31:31 +0330

    structure core views

--- staffy/Core/views.py
+++ staffy/Core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request,'Base.html')
+
+def register ():
+    pass
+
+def user_login ():
+    pass
+
+def user_logout ():
+    pass
